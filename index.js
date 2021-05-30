427c4c20d572adf4fa2275cdf87dd1498ae27a36
Samantha Ostrowski <isphinxs@hotmail.com>
2021-05-29 20:33:18 -0400

    commit tree 8aa0c0459b8a7b7a5cf16405fc25d8cac8d43379

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        // console.log(object);
+        const id = object.id;
+        document.body.innerHTML += id
+    })
+    .catch(function(error) {
+        document.body.innerHTML += error.message
+    });
+}
